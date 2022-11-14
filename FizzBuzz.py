4893719a7002806b69e3ba11f84e7edeff4d0e74
Gabriel Calçada <gmcalcada@gmail.com>
2022-11-14 16:00:16 -0300

    Refatorando o código e documentando

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,8 +1,4 @@
-"""Se o número for múltiplo de 3, imprime Fizz.
-Se o número for múltiplo de 5, imprime Buzz
-Se o número for múltiplo de 3 E 5, imprime FizzBuzz
-Se não for nenhum, imprime o próprio número"""
-
+"Sem função primeiramente"
 i = int(input(("Digite o número: ")))
 if (i%3 == 0 and i%5 == 0):
     print("FizzBuzz")
